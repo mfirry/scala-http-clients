35f3df59519171bacb737f5aa3ed059bd7227479
Filippo Vitale <fvitale@arbor.net>
2017-05-22 11:37:46 -0400

    comment out the problematic line

--- lolhttp/src/main/scala/com/example/Lolhttp.scala
+++ lolhttp/src/main/scala/com/example/Lolhttp.scala
@@ -10,6 +10,6 @@ object Lolhttp extends App {
   val githubClient = Client("api.github.com", 443, "https")
   val request = Get("/users/scala-italy").addHeaders(h"User-Agent" -> h"Awesome-Octocat-App")
 
-  githubClient.run(request)(_.readSuccessAs[String]).foreach(println)
+  // githubClient.run(request)(_.readSuccessAs[String]).foreach(println)
 
 }
