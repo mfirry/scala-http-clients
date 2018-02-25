7ef6da959679432020e59cc73a2416ec09e039e4
Marco F <mfirry@users.noreply.github.com>
2018-02-25 10:55:04 +0100

    minor

--- dispatch/src/main/scala/dispatch/Dispatch.scala
+++ dispatch/src/main/scala/dispatch/Dispatch.scala
@@ -6,14 +6,14 @@ import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 
 // https://dispatchhttp.org
-object Dispatch extends App {
+object Dispatch {
 
-  // def main(args: Array[String]): Unit = {s
+  def main(args: Array[String]): Unit = {
 
     val header = Map("User-Agent" -> "Awesome-Octocat-App")
     val github = url("https://api.github.com/users/scala-italy") appendHeaders header
 
     val x = Await.result(Http(github OK as.String), 1 second)
     println(x)
-  // }
+  }
 }
