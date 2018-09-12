1ab2acdcbc68b650c96612f2d4d9a8e88d2da3ba
Marco F <mfirry@users.noreply.github.com>
2018-09-12 15:50:34 +0200

    minor

--- scalaj/src/main/scala/scalaj/SimplifiedHttp.scala
+++ scalaj/src/main/scala/scalaj/SimplifiedHttp.scala
@@ -2,7 +2,7 @@ package com.example
 import scalaj.http.Http
 
 // https://github.com/scalaj/scalaj-http
-object SimplifiedHttp {
+object SimplifiedHttp extends App {
 
   val response = Http("https://api.github.com/users/scala-italy")
     .header("User-Agent", "Awesome-Octocat-App")
--- sttp/src/main/scala/Hello.scala
+++ sttp/src/main/scala/Hello.scala
@@ -4,4 +4,5 @@ object Hello extends App {
   val request = sttp.get(uri"https://api.github.com/users/scala-italy")
   implicit val handler = HttpURLConnectionBackend()
   val response = request.send()
+  println(response)
 }
