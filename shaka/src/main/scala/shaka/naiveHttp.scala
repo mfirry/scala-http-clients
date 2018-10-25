48ccf59ef3bec5ed994d8f058aef3bc4bb68771e
Marco F <mfirry@users.noreply.github.com>
2018-10-25 11:59:31 +0200

    minor fixes and sbt update

--- shaka/src/main/scala/shaka/naiveHttp.scala
+++ shaka/src/main/scala/shaka/naiveHttp.scala
@@ -5,7 +5,7 @@ import io.shaka.http.Request.{GET, POST}
 import io.shaka.http.HttpHeader.USER_AGENT
 
 // https://github.com/timt/naive-http
-object naiveHttp {
+object naiveHttp extends App {
 
   val response = http(
       GET("https://api.github.com/users/scala-italy").header(
