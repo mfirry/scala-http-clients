ae76c91d5d5fec48711489a5458ad83114da5676
Marco F <mfirry@users.noreply.github.com>
2021-01-13 15:53:16 +0100

    sttp3

--- sttp/src/main/scala/Hello.scala
+++ sttp/src/main/scala/Hello.scala
@@ -1,8 +1,9 @@
-import com.softwaremill.sttp._
+import sttp.client3._
 
 object Hello extends App {
-  val request = sttp.get(uri"https://api.github.com/users/scala-italy")
-  implicit val handler = HttpURLConnectionBackend()
-  val response = request.send()
+  val backend = HttpURLConnectionBackend()
+  val response = basicRequest
+    .get(uri"https://api.github.com/users/scala-italy")
+    .send(backend)
   println(response)
 }
