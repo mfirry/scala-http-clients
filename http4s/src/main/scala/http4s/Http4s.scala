e8c144ec3d3154fa314953c73565f14c709693a4
Marco F <mfirry@users.noreply.github.com>
2021-09-27 17:26:34 +0200

    http4s 0.23.4

--- http4s/src/main/scala/http4s/Http4s.scala
+++ http4s/src/main/scala/http4s/Http4s.scala
@@ -3,8 +3,8 @@ package com.example
 import java.util.concurrent._
 
 import cats.effect._
-import cats.effect.Blocker
 
+import org.typelevel.ci._
 import org.http4s.{Header, Http, Request, Response, Uri}
 import org.http4s.Method.GET
 import org.http4s._
@@ -16,15 +16,14 @@ object Http4s extends cats.effect.IOApp {
 
   def run(args: List[String]): IO[ExitCode] = {
     val blockingPool = Executors.newFixedThreadPool(5)
-    val blocker = Blocker.liftExecutorService(blockingPool)
-    val httpClient: Client[IO] = JavaNetClientBuilder[IO](blocker).create
+    val httpClient: Client[IO] = JavaNetClientBuilder[IO].create
 
     val uri = uri"https://api.github.com/users/scala-italy"
 
     httpClient
       .expect[String](
         Request[IO](method = GET, uri = uri)
-          .withHeaders(Header("User-Agent", "Awesome-Octocat-App"))
+          .withHeaders(Header.Raw(ci"User-Agent", "Awesome-Octocat-App"))
       )
       .as(ExitCode.Success)
 
